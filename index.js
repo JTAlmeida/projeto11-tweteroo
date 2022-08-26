a66adad816b9b13336aad6a8c792012cc0d4f8be
Jonatas <jonatasalmeidasm@gmail.com>
2022-08-26 08:54:51 -0300

    all post and get methods done and tested (no bonus)

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ import express from "express";
 import cors from "cors";
 
 const server = express();
-
+server.use(express.json());
 server.use(cors());
 
-server.get("/", (req, res) => {
-  res.send("Teste inicial");
+const tweets = [];
+const userData = [];
+
+server.post("/sign-up", (req, res) => {
+  userData.push(req.body);
+  res.send(userData);
+});
+
+server.post("/tweets", (req, res) => {
+  tweets.push(req.body);
+  res.send(tweets);
+});
+
+server.get("/tweets", (req, res) => {
+  const latestTweets = tweets.slice(-10);
+
+  const userTweets = latestTweets.map((tweet) => {
+    for (let i = 0; i < userData.length; i++) {
+      if (tweet.username === userData[i].username) {
+        return {...tweet, avatar: userData[i].avatar };
+      }
+    }
+  });
+  res.send(userTweets);
 });
 
 server.listen(5000);
